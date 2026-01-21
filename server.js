5230863b1c70da2a13f63460d25c26d298686271
Gio-Padilla <pad24005@byui.edu>
2026-01-21 15:50:12 -0600

    W03 Learning Activities

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +26,16 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+/* ***********************OLD////
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
+ *************************/
 
 /* ***********************
  * Local Server Information
